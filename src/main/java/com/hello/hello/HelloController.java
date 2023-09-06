fea82810821de72f810fac46df14118a9635e2cb
”simamumu89” <t.shima89mac.pro@gmail.com>
2023-09-06 23:15:10 +0900

    ListでStudentを追加

--- src/main/java/com/hello/hello/HelloController.java
+++ src/main/java/com/hello/hello/HelloController.java
@@ -3,11 +3,18 @@ package com.hello.hello;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.Map;
+
 @RestController
 public class HelloController {
 
     @GetMapping("/hello")
-    public  String hello(){
-        return "Hello World!";
+    public List<Student> hello(){
+        List<Student> student = List.of(
+                new Student(1, "Yamada", 25, "standing work", "sprain"),
+                new Student(2, "Shimamura",27, "desk work", "lower back pain"),
+                new Student(3, "Murakami", 29, "baseball player", "Shoulder pain"));
+        return student;
     }
 }
---
+++ src/main/java/com/hello/hello/Student.java
@@ -0,0 +1,42 @@
+package com.hello.hello;
+
+public class Student {
+
+    private int id;
+
+    private String name;
+
+    private int age;
+
+    private String Profession;
+
+    private String DiseaseName;
+
+    public Student(int id, String name, int age, String profession, String diseaseName) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        Profession = profession;
+        DiseaseName = diseaseName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getProfession() {
+        return Profession;
+    }
+
+    public String getDiseaseName() {
+        return DiseaseName;
+    }
+}
